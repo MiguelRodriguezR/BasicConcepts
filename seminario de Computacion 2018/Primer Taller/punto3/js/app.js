2740485bf2895670e8be0a6bdef4226da41f2503
MiguelRodriguezR <miguel.a.rodriguez.r@gmail.com>
2018-04-04 11:43:12 -0500

    punto 3 taller 1 seminario computacion

---
+++ seminario de Computacion 2018/Primer Taller/punto3/js/app.js
@@ -0,0 +1,44 @@
+var slideIndex = 2;
+
+var descriptions = [
+  "<b>Monster 1200s</b><br> Marca : Ducati<br>Cilindraje : 1200cc<br>Uso: Traveling",
+  "<b>R1</b><br> Marca : Yamaha<br>Cilindraje : 998cc<br>Uso: Competencia",
+  "<b>XJ6</b><br> Marca : Yamaha<br>Cilindraje : 600cc<br>Uso: City Bike - Naked",
+  "<b>R6</b><br> Marca : Yamaha<br>Cilindraje : 599cc<br>Uso: City Bike",
+  "<b>Ninja</b><br> Marca : Kawasaki<br>Cilindraje : 300cc<br>Uso: City Bike",
+  "<b>Monster 796s</b><br> Marca : Yamaha<br>Cilindraje : 796cc<br>Uso: Traveling",
+]
+
+
+
+window.onload = function() {
+  showSlides(slideIndex);
+}
+
+function showSlides(slide) {
+  var slider = document.querySelector("#slider");
+
+  var posLeft = slide-1;
+  var posRight = slide+1;
+  if(slide==0){
+    posLeft = descriptions.length-1;
+  }
+  if(slide==descriptions.length-1){
+    posRight = 0;
+  }
+  var left = "<img class='left' src='images/p"+posLeft+".jpg' >";
+  var actual = "<img class='actual' src='images/p"+slide+".jpg' >";
+  var right = "<img class='right' src='images/p"+posRight+".jpg' >";
+  slider.innerHTML=left+actual+right;
+
+  var descContainer = document.querySelector(".desc-Container");
+  descContainer.innerHTML="<h3>"+descriptions[slide]+"</h3>"
+}
+
+function pasar(towhere){
+  if(towhere=="-1" && slideIndex==0) slideIndex=descriptions.length-1;
+  else if(towhere=="1" && slideIndex==descriptions.length-1) slideIndex=0;
+  else if(towhere=="1") slideIndex++;
+  else slideIndex--;
+  showSlides(slideIndex);
+}
